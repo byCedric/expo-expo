7512f57e89ad022cc579a2ccbe04f95c96ded946
Test User <gosimek@gmail.com>
2021-08-17 18:18:27 +0200

    further improvements, APIGridCell component extracted

--- docs/ui/components/home/GridCell.tsx
+++ docs/ui/components/home/GridCell.tsx
@@ -1,38 +1,85 @@
-import { css, SerializedStyles, Theme } from '@emotion/react';
-import { Interpolation } from '@emotion/serialize';
+import { css, SerializedStyless } from '@emotion/react';
 import { borderRadius, theme } from '@expo/styleguide';
 import React, { PropsWithChildren } from 'react';
 import { Col, ColProps } from 'react-grid-system';
 
+import { Link } from '~/ui/components/link';
+import { fontStacks } from '~/ui/foundations/typography';
+
 type GridCellProps = ColProps & {
-  style?: SerializedStyles | Interpolation<Theme>;
+  style?: SerializedStyles;
 };
 
-export const GridCell = ({
-  children,
-  md,
-  style = gridCellDefaultStyle,
-}: PropsWithChildren<GridCellProps>) => (
-  <Col css={gridCellWrapperStyle} md={md}>
-    <div css={[gridCellStyle, style]}>{children}</div>
+type APIGridCellProps = GridCellProps & {
+  icon?: string;
+  title?: string;
+  link?: string;
+};
+
+export const GridCell = ({ children, md, style = {} }: PropsWithChildren<GridCellProps>) => (
+  <Col css={cellWrapperStyle} md={md}>
+    <div css={[cellStyle, style]}>{children}</div>
   </Col>
 );
 
-const gridCellWrapperStyle = css`
+export const APIGridCell = ({ md, icon, title, link, style = {} }: APIGridCellProps) => (
+  <Col css={cellWrapperStyle} md={md}>
+    <div css={[cellStyle, cellAPIStyle, style]}>
+      <>
+        <div css={cellIconWrapperStyle}>{icon}</div>
+        {title && link && (
+          <div css={cellTitleWrapperStyle}>
+            <Link href={link} css={cellTitleStyle}>
+              {title}
+              <span css={cellTitleArrow}>{'->'}</span>
+            </Link>
+          </div>
+        )}
+      </>
+    </div>
+  </Col>
+);
+
+const cellWrapperStyle = css`
   padding-left: 0 !important;
   padding-right: 0 !important;
 `;
 
-const gridCellStyle = {
+const cellStyle = css({
   borderRadius: borderRadius.large,
   margin: 16,
-  padding: 16,
+  padding: 32,
   minHeight: 200,
-};
+});
 
-const gridCellDefaultStyle = {
+const cellAPIStyle = css({
   backgroundColor: theme.background.secondary,
   borderWidth: 1,
   borderStyle: 'solid',
   borderColor: theme.border.default,
-};
+  padding: 0,
+  overflow: 'hidden',
+});
+
+const cellIconWrapperStyle = css({
+  display: 'flex',
+  minHeight: 136,
+  justifyContent: 'space-around',
+  alignItems: 'center',
+});
+
+const cellTitleWrapperStyle = css({
+  display: 'flex',
+  backgroundColor: theme.background.default,
+  padding: 16,
+});
+
+const cellTitleStyle = css({
+  fontSize: 15,
+  fontFamily: fontStacks.bold,
+  lineHeight: '30px',
+  color: theme.text.default,
+  textDecoration: 'none',
+});
+
+const cellTitleArrow = css({ float: 'right', fontSize: 18, color: theme.text.secondary });
